bc067d5e93bb0a770ebc0b61f34b6015bb9b27b6
陈征 <1007324849@qq.com>
2021-09-13 12:17:50 +0800

    first commit

---
+++ src/main/java/com/zheng/zhengstartuppage/ZhengStartupPageApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.zhengstartuppage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhengStartupPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhengStartupPageApplication.class, args);
+    }
+
+}
