cc943ab34b426f2c927504c341dfc13df8b5677a
陈征 <1007324849@qq.com>
2021-10-03 22:03:36 +0800

    返回类限定 *Entity

---
+++ src/main/java/com/zheng/zhengstartuppage/exception/IllegalResultClassException.java
@@ -0,0 +1,12 @@
+package com.zheng.zhengstartuppage.exception;
+
+/**
+ * @author : 陈征
+ * @date : 2021-10-03 20:33
+ */
+
+public class IllegalResultClassException extends Exception {
+    public IllegalResultClassException(String message) {
+        super(message);
+    }
+}
