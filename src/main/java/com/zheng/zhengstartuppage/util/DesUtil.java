780c5d7350e59d91a0ff6aa929ea8b47d93558fd
陈征 <1007324849@qq.com>
2021-10-10 19:50:59 +0800

    测试des

---
+++ src/main/java/com/zheng/zhengstartuppage/util/DesUtil.java
@@ -0,0 +1,45 @@
+package com.zheng.zhengstartuppage.util;
+
+import com.zheng.zhengstartuppage.config.DesConfig;
+import com.zheng.zhengstartuppage.config.WebApplicationContextConfig;
+
+import javax.crypto.*;
+import javax.crypto.spec.DESKeySpec;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Date;
+
+/**
+ * @author : 陈征
+ * @date : 2021-10-10 19:39
+ */
+
+public class DesUtil {
+    public static final byte[] keys = WebApplicationContextConfig.getBean(DesConfig.class).key;
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    private static Cipher encryptCipher;
+
+    private static Cipher decryptCipher;
+
+    static {
+        try {
+            SecretKey secretKey = SecretKeyFactory.getInstance("DES").generateSecret(new DESKeySpec(keys));
+            encryptCipher = Cipher.getInstance("DES");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, secretKey, secureRandom);
+            decryptCipher = Cipher.getInstance("DES");
+            decryptCipher.init(Cipher.DECRYPT_MODE, secretKey, secureRandom);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String encode(String source) throws IllegalBlockSizeException, BadPaddingException {
+        return Base64.getEncoder().encodeToString(encryptCipher.doFinal(source.getBytes()));
+    }
+
+    public static String decode(String source) throws IllegalBlockSizeException, BadPaddingException {
+        return new String(decryptCipher.doFinal(Base64.getDecoder().decode(source)));
+    }
+}
