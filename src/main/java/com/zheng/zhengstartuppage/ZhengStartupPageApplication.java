f5eba8f98feb3a9301404dbc25172e1c04f5f425
陈征 <1007324849@qq.com>
2021-12-15 22:12:13 +0800

    移除qq机器人的依赖

--- src/main/java/com/zheng/zhengstartuppage/ZhengStartupPageApplication.java
+++ src/main/java/com/zheng/zhengstartuppage/ZhengStartupPageApplication.java
@@ -1,10 +1,8 @@
 package com.zheng.zhengstartuppage;
 
-import love.forte.simbot.spring.autoconfigure.EnableSimbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableSimbot
 @SpringBootApplication
 public class ZhengStartupPageApplication {
     public static void main(String[] args) {
